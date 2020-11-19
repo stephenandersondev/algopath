f7bd380fadc12b41d4f79e2510bbed37b9cf689a
stephenandersondev <stephenandersondev@gmail.com>
2020-11-19 17:40:13 -0600

    built visualizer foundation

---
+++ algopath-frontend/src/components/Node/Node.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import './Node.css'
+
+export default class Node extends Component {
+
+    render() {
+
+        const {
+            col,
+            row,
+            isWall,
+            isStart,
+            isFinish,
+            onMouseDown,
+            onMouseEnter,
+            onMouseUp
+        } = this.props;
+
+        const extraClassName =
+            isFinish
+                ? 'node-finish'
+                : isStart
+                    ? 'node-start'
+                    : isWall
+                        ? 'node-wall'
+                        : '';
+
+        return (
+            <div
+                id={`node-${row}-${col}`}
+                className={`node ${extraClassName}`}
+                onMouseDown={() => onMouseDown(row, col)}
+                onMouseEnter={() => onMouseEnter(row, col)}
+                onMouseUp={() => onMouseUp()}>
+            </div>
+        )
+    }
+}
